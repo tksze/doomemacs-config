3bf859588e07c071f0939a5a86901b12e3a5c836
tksze <jostszkit.s@gmail.com>
2023-01-18 22:53:28 +0800

    change: Also maximize frame in Windows

--- config.el
+++ config.el
@@ -29,9 +29,7 @@
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
 
-(when (eq system-type 'darwin)
-    (add-hook 'window-setup-hook #'toggle-frame-maximized)
-)
+(add-hook 'window-setup-hook #'toggle-frame-maximized)
 
 ;; The chinese font size please be x1.2 of latin font, other wise the line
 ;; height will be incompatible
